02775419737bb245d5618e79d0c0356542501606
Jelle De Loecker <jelle@elevenways.be>
2021-12-03 18:20:00 +0100

    Add mixin for handling string inputs

---
+++ src/main/java/rocks/blackblock/screenbuilder/mixin/ServerPlayNetworkHandlerMixin.java
@@ -0,0 +1,28 @@
+package rocks.blackblock.screenbuilder.mixin;
+
+import net.minecraft.SharedConstants;
+import net.minecraft.network.packet.c2s.play.RenameItemC2SPacket;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import rocks.blackblock.screenbuilder.TexturedScreenHandler;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public abstract class ServerPlayNetworkHandlerMixin {
+
+    @Shadow
+    public ServerPlayerEntity player;
+
+    @Inject(at = @At("HEAD"), method="onRenameItem", cancellable = true)
+    public void onRenameItem(RenameItemC2SPacket packet, CallbackInfo ci) {
+        if (this.player.currentScreenHandler instanceof TexturedScreenHandler handler) {
+            String string = SharedConstants.stripInvalidChars(packet.getName());
+            handler.onRenameItem(string);
+            ci.cancel();
+        }
+    }
+}
